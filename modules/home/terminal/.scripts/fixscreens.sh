f2b0357d79fb87141e61988b0cb4d0f00405bbb2
DoctorJax <totallyadev@putering.com>
2024-09-12 22:52:32 -0400

    don't need all them scripts

--- modules/home/terminal/.scripts/fixscreens.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-xrandr --output DP-1 --left-of HDMI-0
-
-xrandr --output HDMI-0 --primary
-
-xsetwacom set "Wacom Bamboo Pen Pen stylus" MapToOutput HEAD-0
-
-xset r rate 600 75
--- modules/home/terminal/scripts.nix
+++ modules/home/terminal/scripts.nix
@@ -1,6 +1,14 @@
 {config, lib, pkgs, ...}: {
   home.packages = [
     pkgs.libnotify
+    pkgs.headsetcontrol
+    pkgs.alsa-utils
+    pkgs.ffmpeg
+    pkgs.grimblast
+    pkgs.maim
+    pkgs.ydotool
+    pkgs.feh
+    pkgs.swaybg
   ];
 
   home.file.".scripts" = {
