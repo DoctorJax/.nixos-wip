dca0a4a1885dd864c47017b825d6d6af065f12bc
DoctorJax <totallyadev@putering.com>
2024-09-12 22:12:42 -0400

    ah yes, recursive importing

--- modules/home/terminal/default.nix
+++ modules/home/terminal/default.nix
@@ -1,10 +1,24 @@
-{...}: {
-  imports = [
-    ./shell.nix
-  ];
-#  # imports by full path without copying to /nix/store
-#  imports = builtins.map (n: toString ./. + "/${n}") (builtins.attrNames (builtins.removeAttrs (builtins.readDir ./.) [(builtins.unsafeGetAttrPos "_" {_ = null;}).file]));
-#  
-#  # copies all files from the current directory to /nix/store and imports from /nix/store
-#  # imports = builtins.map (n: "${./.}/${n}") (builtins.attrNames (builtins.removeAttrs (builtins.readDir ./.) [(builtins.unsafeGetAttrPos "_" {_ = null;}).file]));
+{ lib, ... }:
+
+with lib;
+
+let
+
+  # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
+  getDir = dir: mapAttrs (file: type:
+    if type == "directory" then getDir "${dir}/${file}" else type
+  ) (builtins.readDir dir);
+
+  # Collects all files of a directory as a list of strings of paths
+  files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
+
+  # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
+  validFiles = dir: map (file: ./. + "/${file}") (filter (file: hasSuffix ".nix" file && file != "default.nix") (files dir));
+
+in
+
+{
+
+  imports = validFiles ./.;
+
 }
